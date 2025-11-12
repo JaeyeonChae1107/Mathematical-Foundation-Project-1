cf17fa34b980e7278dfac8151cc73c381410edf5
unknown <yachae1107@kentech.ac.kr>
2025-11-12 22:22:58 +0900

    .\protocol1_alice.py

--- Project 1/protocol1_bob.py
+++ Project 1/protocol1_bob.py
@@ -6,32 +6,13 @@ import json
 import random
 
 
-def is_prime(n, k=5):
-    if n <= 1:
+def is_prime(num):
+    if num < 2:
         return False
-    if n <= 3:
-        return True
-    if n % 2 == 0:
-        return False
-
-    r, d = 0, n - 1
-    while d % 2 == 0:
-        r += 1
-        d //= 2
-
-    for _ in range(k):
-        a = random.randrange(2, n - 2)
-        x = pow(a, d, n)
-        if x == 1 or x == n - 1:
-            continue
-        for __ in range(r - 1):
-            x = pow(x, 2, n)
-            if x == n - 1:
-                break
-        else:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
             return False
     return True
-
 
 def gcd(a, b):
     while b:
